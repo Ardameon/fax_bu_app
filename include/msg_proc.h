46165d4aebd205e1e7ea4bca31f82a9f7f68ec59
Ardameon <helldiver@yandex.ru>
2016-01-21 15:43:38 +0600

    fax: msg processing improved

--- include/msg_proc.h
+++ include/msg_proc.h
@@ -4,16 +4,14 @@
 #include <stdint.h>
 #include "fax_bu.h"
 
-#define MSG_STR_SIG_SETUP "SETUP"
-#define MSG_STR_SIG_OK    "OK"
-#define MSG_STR_SIG_ERROR "ERROR"
-
 #define MSG_STR_ERROR_INTERNAL "INTERNAL_ERR"
 #define MSG_STR_ERROR_UNKNOWN  "UNKNOWN_ERR"
 
 #define MSG_STR_MODE_GG "GG"
 #define MSG_STR_MODE_GT "GT"
 #define MSG_STR_MODE_UN "UN"
+
+#define MSG_BUF_LEN_MAX 256
 
 typedef enum {
     FAX_MSG_SETUP,
@@ -51,12 +49,24 @@ typedef struct sig_message_error_t {
     sig_msg_error_e err;
 } sig_message_error_t;
 
+int  msg_createSetup(const char *call_id,
+                     uint32_t src_ip, uint16_t src_port,
+                     uint32_t dst_ip, uint16_t dst_port,
+                     fax_mode_e mode, sig_message_setup_t **msg_setup);
+int  msg_createOk(const char *call_id, uint32_t ip, uint16_t port,
+                  sig_message_ok_t **msg_ok);
+int  msg_createError(const char *call_id, sig_msg_error_e err,
+                  sig_message_error_t **msg_error);
+
 int  msg_bufCreate(const sig_message_t *message, uint8_t **msg_buf);
 void msg_bufDestroy(uint8_t *msg_buf);
 
 int  msg_parse(const uint8_t *msg_buf, sig_message_t **message);
 void msg_destroy(sig_message_t *message);
 
+int  msg_print(const sig_message_t *message, char *buf, int len);
+
 char *ip2str(uint32_t ip, int id);
+const char *msg_msgTypeStr(sig_msg_type_e type);
 
 #endif // MSG_PROC_H
--- src/msg_proc.c
+++ src/msg_proc.c
@@ -8,7 +8,11 @@
 
 #include "msg_proc.h"
 
-#define MESSAGE_BUF_LEN 256
+#define MSG_STR_SIG_SETUP "SETUP"
+#define MSG_STR_SIG_OK    "OK"
+#define MSG_STR_SIG_ERROR "ERROR"
+
+#define MSG_PRINT_BUF_LEN 512
 
 #define IP2STR_MAX 4
 
@@ -20,6 +24,17 @@ static const char *msg_faxModeStr(fax_mode_e mode)
         case FAX_MODE_GW_GW:   return MSG_STR_MODE_GG;
         case FAX_MODE_GW_TERM: return MSG_STR_MODE_GT;
         default:               return MSG_STR_MODE_UN;
+    }
+}
+
+const char *msg_msgTypeStr(sig_msg_type_e type)
+{
+    switch (type)
+    {
+        case FAX_MSG_SETUP:    return MSG_STR_SIG_SETUP;
+        case FAX_MSG_OK:       return MSG_STR_SIG_OK;
+        case FAX_MSG_ERROR:    return MSG_STR_SIG_ERROR;
+        default:               return "UNKNOWN";
     }
 }
 
@@ -51,6 +66,105 @@ char *ip2str(uint32_t ip, int id)
 	return p;
 }
 
+/*============================================================================*/
+
+
+int  msg_createSetup(const char *call_id,
+                     uint32_t src_ip, uint16_t src_port,
+                     uint32_t dst_ip, uint16_t dst_port,
+                     fax_mode_e mode, sig_message_setup_t **msg_setup)
+{
+    int ret_val = 0;
+    sig_message_setup_t *setup = NULL;
+
+    if(!call_id || !msg_setup)
+    {
+        ret_val = -1; goto _exit;
+    }
+
+    setup = calloc(1, sizeof(*setup));
+    if(!setup)
+    {
+        ret_val = -2; goto _exit;
+    }
+
+    setup->src_ip = src_ip;
+    setup->src_port = src_port;
+    setup->dst_ip = dst_ip;
+    setup->dst_port = dst_port;
+    setup->mode = mode;
+    strcpy(setup->msg.call_id, call_id);
+    setup->msg.type = FAX_MSG_SETUP;
+
+    *msg_setup = setup;
+
+_exit:
+    return ret_val;
+}
+
+
+/*============================================================================*/
+
+
+int  msg_createOk(const char *call_id, uint32_t ip, uint16_t port,
+                  sig_message_ok_t **msg_ok)
+{
+    int ret_val = 0;
+    sig_message_ok_t *ok = NULL;
+
+    if(!call_id || !msg_ok)
+    {
+        ret_val = -1; goto _exit;
+    }
+
+    ok = calloc(1, sizeof(*ok));
+    if(!ok)
+    {
+        ret_val = -2; goto _exit;
+    }
+
+    ok->ip = ip;
+    ok->port = port;
+    strcpy(ok->msg.call_id, call_id);
+    ok->msg.type = FAX_MSG_OK;
+
+    *msg_ok = ok;
+
+_exit:
+    return ret_val;
+}
+
+
+/*============================================================================*/
+
+
+int  msg_createError(const char *call_id, sig_msg_error_e err,
+                  sig_message_error_t **msg_error)
+{
+    int ret_val = 0;
+    sig_message_error_t *error = NULL;
+
+    if(!call_id || !msg_error)
+    {
+        ret_val = -1; goto _exit;
+    }
+
+    error = calloc(1, sizeof(*error));
+    if(!error)
+    {
+        ret_val = -2; goto _exit;
+    }
+
+    error->err = err;
+    strcpy(error->msg.call_id, call_id);
+    error->msg.type = FAX_MSG_ERROR;
+
+    *msg_error = error;
+
+_exit:
+    return ret_val;
+}
+
 
 /*============================================================================*/
 
@@ -64,8 +178,8 @@ static int msg_bufCreateSetup(const sig_message_setup_t *message,
     {
         case FAX_MODE_GW_GW:
             len = snprintf((char *)msg_buf,
-                           MESSAGE_BUF_LEN, "%s %s %s %s:%u %s:%u\r\n",
-                           MSG_STR_SIG_SETUP, message->msg.call_id,
+                           MSG_BUF_LEN_MAX, "%s %s %s %s:%u %s:%u\r\n",
+                           msg_msgTypeStr(FAX_MSG_SETUP), message->msg.call_id,
                            msg_faxModeStr(message->mode),
                            ip2str(message->src_ip, 0), message->src_port,
                            ip2str(message->dst_ip, 1), message->dst_port);
@@ -73,8 +187,8 @@ static int msg_bufCreateSetup(const sig_message_setup_t *message,
 
         case FAX_MODE_GW_TERM:
             len = snprintf((char *)msg_buf,
-                           MESSAGE_BUF_LEN, "%s %s %s %s:%u\r\n",
-                           MSG_STR_SIG_SETUP, message->msg.call_id,
+                           MSG_BUF_LEN_MAX, "%s %s %s %s:%u\r\n",
+                           msg_msgTypeStr(FAX_MSG_SETUP), message->msg.call_id,
                            msg_faxModeStr(message->mode),
                            ip2str(message->src_ip, 0), message->src_port);
             break;
@@ -93,8 +207,8 @@ static int msg_bufCreateError(const sig_message_error_t *message,
 {
     int len = 0;
 
-    len = snprintf((char *)msg_buf, MESSAGE_BUF_LEN, "%s %s %s\r\n",
-                   MSG_STR_SIG_ERROR, message->msg.call_id,
+    len = snprintf((char *)msg_buf, MSG_BUF_LEN_MAX, "%s %s %s\r\n",
+                   msg_msgTypeStr(FAX_MSG_ERROR), message->msg.call_id,
                    msg_errStr(message->err));
 
     return len;
@@ -105,8 +219,8 @@ static int msg_bufCreateOk(const sig_message_ok_t *message, uint8_t *msg_buf)
 {
     int len = 0;
 
-    len = snprintf((char *)msg_buf, MESSAGE_BUF_LEN, "%s %s %s:%u\r\n",
-                   MSG_STR_SIG_OK, message->msg.call_id,
+    len = snprintf((char *)msg_buf, MSG_BUF_LEN_MAX, "%s %s %s:%u\r\n",
+                   msg_msgTypeStr(FAX_MSG_OK), message->msg.call_id,
                    ip2str(message->ip, 0), message->port);
 
     return len;
@@ -123,7 +237,7 @@ int  msg_bufCreate(const sig_message_t *message, uint8_t **msg_buf)
         ret_val = -1; goto _exit;
     }
 
-    buf = calloc(MESSAGE_BUF_LEN, sizeof(**msg_buf));
+    buf = calloc(MSG_BUF_LEN_MAX, sizeof(**msg_buf));
     if(!buf)
     {
         ret_val = -2; goto _exit;
@@ -324,14 +438,14 @@ int msg_parse(const uint8_t *msg_buf, sig_message_t **message)
     msg_payload = (char *)
             (msg_buf + strlen(msg_type_str) + strlen(call_id) + 2);
 
-    if(!strcmp(msg_type_str, MSG_STR_SIG_SETUP))
+    if(!strcmp(msg_type_str, msg_msgTypeStr(FAX_MSG_SETUP)))
     {
         res = msg_parseSetup(msg_payload, (sig_message_setup_t **)message);
         msg_type = FAX_MSG_SETUP;
-    } else if(!strcmp(msg_type_str, MSG_STR_SIG_OK)) {
+    } else if(!strcmp(msg_type_str, msg_msgTypeStr(FAX_MSG_OK))) {
         res = msg_parseOk(msg_payload, (sig_message_ok_t **)message);
         msg_type = FAX_MSG_OK;
-    } else if(!strcmp(msg_type_str, MSG_STR_SIG_ERROR)) {
+    } else if(!strcmp(msg_type_str, msg_msgTypeStr(FAX_MSG_ERROR))) {
         res = msg_parseError(msg_payload, (sig_message_error_t **)message);
         msg_type = FAX_MSG_ERROR;
     } else{
@@ -362,7 +476,89 @@ void msg_destroy(sig_message_t *message)
 
 /*============================================================================*/
 
+static int msg_printSetup(const sig_message_setup_t *message, char *buf)
+{
+    sprintf(buf,
+            "\t src:     %s:%u\n"
+            "\t dst:     %s:%u\n"
+            "\t mode:    %s\n",
+            ip2str(message->src_ip, 0), message->src_port,
+            ip2str(message->dst_ip, 1), message->dst_port,
+            msg_faxModeStr(message->mode));
 
+    return 0;
+}
+
+
+static int msg_printOk(const sig_message_ok_t *message, char *buf)
+{
+    sprintf(buf,
+            "\t addr:    %s:%u\n",
+            ip2str(message->ip, 0), message->port);
+
+    return 0;
+}
+
+
+static int msg_printError(const sig_message_error_t *message, char *buf)
+{
+    sprintf(buf,
+            "\t error:   %s\n",
+            msg_errStr(message->err));
+
+    return 0;
+}
+
+
+int msg_print(const sig_message_t *message, char *buf, int len)
+{
+    int ret_val = 0;
+    char tmp_buf[MSG_PRINT_BUF_LEN];
+    char *p;
+
+
+    if(!message || !buf)
+    {
+        sprintf(tmp_buf, "PRINT_ERROR\n");
+        ret_val = -1; goto _exit;
+    }
+
+    sprintf(tmp_buf,
+            "%s:\n"
+            "\t call_id: '%s'\n",
+            msg_msgTypeStr(message->type),
+            message->call_id);
+
+    p = tmp_buf + strlen(tmp_buf);
+
+    switch(message->type)
+    {
+        case FAX_MSG_SETUP:
+            msg_printSetup((sig_message_setup_t *)message, p);
+            break;
+
+        case FAX_MSG_OK:
+            msg_printOk((sig_message_ok_t *)message, p);
+            break;
+
+        case FAX_MSG_ERROR:
+            msg_printError((sig_message_error_t *)message, p);
+            break;
+
+        default:
+            ret_val = -2;
+            sprintf(p, "\t PRINT_ERROR\n");
+            goto _exit;
+    }
+
+    strncpy(buf, tmp_buf, len);
+
+_exit:
+    return ret_val;
+}
+
+
+/*============================================================================*/
 
 
 
